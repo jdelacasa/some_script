01418d7d9014b274e259ba007cae75659a2c6393
root <root@d1stkfactory>
2014-09-27 08:53:24 +0000

    add util_masscan

---
+++ services_docker/utils_masscan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#elasticsearch_dir="/opt/elasticsearch-data"
+workdir="$HOME/workspace/masscan-data"
+
+#
+# create persisten directory for elasticsearch
+#
+
+mkdir -p ${workdir}
+
+
+# build docker from github
+
+docker build -t="jdelacasa/masscan" github.com/jdelacasa/docker_masscan
+
+
+echo "--------"
+echo "masscan installed"
+echo "persisten data in: ${workdir}"
+echo "use: docker run -ti jdelacasa/masscan  masscan -p80,8000-8100 10.0.0.0/8 --rate=10000
