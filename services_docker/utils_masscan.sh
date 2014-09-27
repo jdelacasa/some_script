a45d39ddd78855794b2ac3195dedb8cbd8725da9
root <root@d1stkfactory>
2014-09-27 08:56:32 +0000

    fix

--- services_docker/utils_masscan.sh
+++ services_docker/utils_masscan.sh
@@ -18,4 +18,4 @@ docker build -t="jdelacasa/masscan" github.com/jdelacasa/docker_masscan
 echo "--------"
 echo "masscan installed"
 echo "persisten data in: ${workdir}"
-echo "use: docker run -ti jdelacasa/masscan  masscan -p80,8000-8100 10.0.0.0/8 --rate=10000
+echo "use: docker run -ti jdelacasa/masscan  masscan -p80,8000-8100 10.0.0.0/8 --rate=10000"
